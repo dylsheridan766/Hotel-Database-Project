7bd3f3620f1442a82e1164eb9bfed6506a9ec6f0
dylsheridan766 <dylsheridan@gmail.com>
2026-04-08 16:17:13 -0400

    Reads properties file in test.java

--- Data base project/data-base-project/src/main/java/com.hoteldatabase/hoteldatabase/Test.java
+++ Data base project/data-base-project/src/main/java/com.hoteldatabase/hoteldatabase/Test.java
@@ -1,7 +1,10 @@
 package com.hoteldatabase.hoteldatabase;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
 
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -10,18 +13,29 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet("/testdb")
 public class Test extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String url = "jdbc:postgresql://localhost:5432/postgres?currentSchema=databaseProject"; 
-        String user = "postgres";
-        String password = "";
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+       Properties prop = new Properties();
+try (InputStream input = getServletContext().getResourceAsStream("/WEB-INF/classes/db.properties")) {
+    if (input == null) {
+        out.println("unable to find db.properties");
+        return;
+    }
+    // Load the properties file
+    prop.load(input);
 
-        try {
-            Class.forName("org.postgresql.Driver");
-            Connection conn = DriverManager.getConnection(url, user, password);
-            response.getWriter().println("Success! Connected to PostgreSQL.");
-            conn.close();
-        } catch (Exception e) {
-            response.getWriter().println("Error: " + e.getMessage());
-            e.printStackTrace();
-        }
+    // Get the values
+    String url = prop.getProperty("db.url");
+    String user = prop.getProperty("db.user");
+    String pass = prop.getProperty("db.password");
+
+    // Connect
+    Class.forName("org.postgresql.Driver");
+    Connection conn = DriverManager.getConnection(url, user, pass);
+    out.println("<h1>Successuly connected using properties file.</h1>");
+    conn.close();
+} catch (Exception e) {
+   out.println("<h3>Error:</h3>");
+}
     }
 }
